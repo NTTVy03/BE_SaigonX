ef91cbf6fb0a7a2d0aa5c70f3e6d1a7700df2fea
quangphat18ti <quangphat18ti@gmail.com>
2024-07-27 01:22:20 +0700

    feat: add more function for object type

--- app/type/enum/ObjectType.js
+++ app/type/enum/ObjectType.js
@@ -4,5 +4,24 @@ const ObjectType = {
     CHECKPOINT: 'checkpoint',
     GAME: 'game',
 };
-  
-  module.exports = ObjectType;
+
+const ObjectTypeKeys = Object.keys(ObjectType);
+
+const getObjectType = (type) => {
+    for (let key of ObjectTypeKeys) {
+        if (type === ObjectType[key]) {
+            return key;
+        }
+    }
+    return null;
+}
+
+const ObjectTypeManager = {
+    ObjectTypeKeys,
+    getObjectType
+}
+
+module.exports = {
+    ObjectType,
+    ObjectTypeManager
+}
